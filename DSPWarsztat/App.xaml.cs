300649cd11aed60ac98870aa0ea8c11568a18fbd
Mateusz Malinowski <mateuscod88@gmail.com>
2016-04-02 01:14:43 +0200

    Pierwsze Prace

--- DSPWarsztat/App.xaml.cs
+++ DSPWarsztat/App.xaml.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-
+using DSPWarsztat.ViewModel;
+using DSPWarsztat.Model;
+using System.Collections.ObjectModel;
 namespace DSPWarsztat
 {
     /// <summary>
@@ -13,5 +15,16 @@ namespace DSPWarsztat
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ObservableCollection<CarModel> carsy = new ObservableCollection<CarModel>();
+            carsy.Add(new CarModel("Audi", "A4", "2004", "BIA8406"));
+            carsy.Add(new CarModel("VW", "Passat", "2000", "BIA00568"));
+            MainWindow mw = new MainWindow();
+            AllCarsViewModel lvm = new AllCarsViewModel(carsy);
+            mw.DataContext = lvm;
+            mw.Show();
+        }
     }
 }
