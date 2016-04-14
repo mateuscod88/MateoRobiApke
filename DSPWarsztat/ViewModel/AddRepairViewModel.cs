ab52d9cb96737b2ba2792358fc05fa35b12503a8
Mateusz Malinowski <mateuscod88@gmail.com>
2016-04-14 19:26:56 +0200

    now all view added

---
+++ DSPWarsztat/ViewModel/AddRepairViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSPWarsztat.Model;
+namespace DSPWarsztat.ViewModel
+{
+    class AddRepairViewModel
+    {
+        public RepairModel repairModel { get; set; }
+
+    }
+}
