38bd10aff0e42c03e3c1a82100be8c35f55a7c2c
Mateo <mateuscod88@gmail.com>
2016-04-15 23:16:45 +0200

    Command

--- DSPWarsztat/ViewModel/AddRepairViewModel.cs
+++ DSPWarsztat/ViewModel/AddRepairViewModel.cs
@@ -8,6 +8,18 @@ namespace DSPWarsztat.ViewModel
 {
     class AddRepairViewModel
     {
+		private ICommand backToPreviousWin;
+		private ICommand addRepair;
+		public ICommand AddRepair
+		{
+			get
+			{
+				if(addRepair == null)
+				{
+					addRepair = new RelayCommand(param =>)
+				}
+			}
+		}
         public RepairModel repairModel { get; set; }
 
     }
